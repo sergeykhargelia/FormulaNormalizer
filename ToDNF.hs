3074b3a48441a4827b076b49b808f66c14478e44
sergeykhargelia <71063792+sergeykhargelia@users.noreply.github.com>
2022-03-01 01:54:13 +0300

    fixed a bug in the toDNF function

--- ToDNF.hs
+++ ToDNF.hs
@@ -6,8 +6,8 @@ import ToNNF
 applyDistrLawDNF :: Formula -> Formula
 applyDistrLawDNF (Or f1 f2) = Or (applyDistrLawDNF f1) (applyDistrLawDNF f2)
 applyDistrLawDNF (And f1 f2) = let e = And (applyDistrLawDNF f1) (applyDistrLawDNF f2) in case e of 
-                                   And e1 (Or e2 e3) -> Or (And e1 e2) (And e1 e3)
-                                   And (Or e1 e2) e3 -> Or (And e1 e3) (And e2 e3)
+                                   And e1 (Or e2 e3) -> Or (applyDistrLawDNF (And e1 e2)) (applyDistrLawDNF (And e1 e3))
+                                   And (Or e1 e2) e3 -> Or (applyDistrLawDNF (And e1 e3)) (applyDistrLawDNF (And e2 e3))
                                    otherwise -> e
 applyDistrLawDNF f = f
 
