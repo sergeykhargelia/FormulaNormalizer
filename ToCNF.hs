e1cd4c96a171764e6afc4158bfa9b772e7fb4090
sergeykhargelia <71063792+sergeykhargelia@users.noreply.github.com>
2022-03-01 01:53:42 +0300

    fixed a bug in the toCNF function

--- ToCNF.hs
+++ ToCNF.hs
@@ -6,8 +6,8 @@ import ToNNF
 applyDistrLawCNF :: Formula -> Formula
 applyDistrLawCNF (And f1 f2) = And (applyDistrLawCNF f1) (applyDistrLawCNF f2)
 applyDistrLawCNF (Or f1 f2) = let e = Or (applyDistrLawCNF f1) (applyDistrLawCNF f2) in case e of
-                                  Or e1 (And e2 e3) -> And (Or e1 e2) (Or e1 e3)
-                                  Or (And e1 e2) e3 -> And (Or e1 e3) (Or e2 e3)
+                                  Or e1 (And e2 e3) -> And (applyDistrLawCNF (Or e1 e2)) (applyDistrLawCNF (Or e1 e3))
+                                  Or (And e1 e2) e3 -> And (applyDistrLawCNF (Or e1 e3)) (applyDistrLawCNF (Or e2 e3))
                                   otherwise -> e
 applyDistrLawCNF f = f
 
