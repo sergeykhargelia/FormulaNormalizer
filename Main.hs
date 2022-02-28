f82e1056b44afa02b4deca8700e7d07e87fe0d37
sergeykhargelia <71063792+sergeykhargelia@users.noreply.github.com>
2022-02-28 23:57:10 +0300

    updated main

--- Main.hs
+++ Main.hs
@@ -9,4 +9,17 @@ import Formula
 main :: IO ()
 main = do
     putStr testNNF1
-    
+    putStr testNNF2
+    putStr testNNF3
+    putStr testNNF4
+    putStr testNNF5
+    putStr testDNF1
+    putStr testDNF2
+    putStr testDNF3
+    putStr testDNF4
+    putStr testDNF5
+    putStr testCNF1
+    putStr testCNF2
+    putStr testCNF3
+    putStr testCNF4
+    putStr testCNF5
