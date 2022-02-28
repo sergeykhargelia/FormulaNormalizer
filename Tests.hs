4ec51645c967f9c441447db2189e7577e9b864e8
sergeykhargelia <71063792+sergeykhargelia@users.noreply.github.com>
2022-02-28 23:57:44 +0300

    tests updated

--- Tests.hs
+++ Tests.hs
@@ -6,4 +6,46 @@ import ToDNF
 import ToCNF
 
 testNNF1 :: String
-testNNF1 =  "test 1 passed\n"
+testNNF1 = "test 1 for NNF passed\n"
+
+testNNF2 :: String
+testNNF2 = "test 2 for NNF passed\n"
+
+testNNF3 :: String
+testNNF3 = "test 3 for NNF passed\n"
+
+testNNF4 :: String
+testNNF4 = "test 4 for NNF passed\n"
+
+testNNF5 :: String
+testNNF5 = "test 5 for NNF passed\n"
+
+testDNF1 :: String
+testDNF1 = "test 1 for DNF passed\n"
+
+testDNF2 :: String
+testDNF2 = "test 2 for DNF passed\n"
+
+testDNF3 :: String
+testDNF3 = "test 3 for DNF passed\n"
+
+testDNF4 :: String
+testDNF4 = "test 4 for DNF passed\n"
+
+testDNF5 :: String
+testDNF5 = "test 5 for DNF passed\n"
+
+testCNF1 :: String
+testCNF1 = "test 1 for CNF passed\n"
+
+testCNF2 :: String
+testCNF2 = "test 2 for CNF passed\n"
+
+testCNF3 :: String
+testCNF3 = "test 3 for CNF passed\n"
+
+testCNF4 :: String
+testCNF4 = "test 4 for CNF passed\n"
+
+testCNF5 :: String
+testCNF5 = "test 5 for CNF passed\n"
